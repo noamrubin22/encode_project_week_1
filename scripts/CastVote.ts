c6f245d8a99b2f9a129a19f91c58d7974f734e38
Noam <noamru22@gmail.com>
2023-03-06 13:28:37 +0100

    give right to vote script done

--- scripts/CastVote.ts
+++ scripts/CastVote.ts
@@ -1,3 +1,10 @@
+const main = async () => {};
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
+
 // Get a provider
 
 // Get your signer from .env (should be voter)
--- scripts/GiveRightToVote.ts
+++ scripts/GiveRightToVote.ts
@@ -1,7 +1,37 @@
-// Get a provider
+import { ethers } from "hardhat";
+import * as dotenv from "dotenv";
+import { Ballot__factory } from "../typechain-types";
+dotenv.config();
 
-// Get your signer from .env (should be chairperson)
+const main = async () => {
+  const args = process.argv;
+  const giveRightsToAddress = args[2];
 
-// Receive address of ballot and voter as paramter from CLI
+  // get provider
+  const provider = new ethers.providers.AlchemyProvider(
+    "goerli",
+    process.env.ALCHEMY_API_KEY
+  );
 
-// Create a contract instance  (attach)
+  // connect to wallet using seed phrase
+  const mnemonic = process.env.MNEMONIC;
+  if (!mnemonic || mnemonic.length <= 0)
+    throw new Error("Missing environment: Mnemonic seed");
+
+  const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+  const signer = wallet.connect(provider);
+
+  // get the deployed contract
+  const contract = new Ballot__factory(signer);
+  console.log(`Attaching to ballot contract on address ${signer.address}`);
+  const deployedContract = contract.attach(signer.address);
+  console.log("Successfully attached");
+  console.log(`Giving voting rights to address ${giveRightsToAddress}`);
+  await deployedContract.giveRightToVote(giveRightsToAddress);
+  console.log(`${giveRightsToAddress} can vote now`);
+};
+
+main().catch((error) => {
+  console.log(error);
+  process.exitCode = 1;
+});
